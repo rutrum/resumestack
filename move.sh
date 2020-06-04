924eb4021e7a7287fdf496d7ca98c2586a2b2b4f
David Purdum <purdum41@gmail.com>
2020-06-04 08:17:17 -0400

    Remove old shell scripts

--- move.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Move the pdf to zola-site repository
-PDF_SRC="/home/rutrum/repo/resumestack/resume/DavidPurdumResume.pdf"
-SITE_STATIC="/home/rutrum/repo/zola-site/static/DavidPurdumResume.pdf"
-cp $PDF_SRC $SITE_STATIC
